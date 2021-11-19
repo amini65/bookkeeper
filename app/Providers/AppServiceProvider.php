8be094f4a09b9aa5af507ef1d01615bb49eae6dd
amini65 <amir.amini65@gmail.com>
2021-11-19 13:59:21 +0330

    create panel master page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->loadViewsFrom(resource_path('views/panel'),'Dashboard');
     }
 
     /**
